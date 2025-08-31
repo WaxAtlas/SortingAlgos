26db18da93ae0e9eaa4442de91609ee3aaf9b5c5
Watson Carawon <carawonw@gmail.com>
2025-08-31 16:17:59 -0400

    Add bubble sort

---
+++ BubbleSort.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿namespace BubbleSort.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BubbleTest_Null_ReturnNull()
+        {
+            int[]? arr = null;
+            Bubble.sort(arr, 0);
+            Assert.That(arr, Is.Null);
+        }
+        [Test]
+        public void BubbleTest_Empty_ReturnEmpty()
+        {
+            int[] arr = [];
+            Bubble.sort(arr, arr.Length);
+            Assert.That(arr, Has.Length.EqualTo(0));
+        }
+        [Test]
+        public void BubbleTest_ArrayWithOneElement_ReturnArrayWithOneElement()
+        {
+            int[] arr = [4];
+            Bubble.sort(arr, arr.Length);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(arr, Has.Length.EqualTo(1));
+                Assert.That(arr, Has.Exactly(1).EqualTo(4));
+            }
+        }
+        [Test]
+        public void BubbleTest_ArrayWithRangeTo10_ReturnSortedArray()
+        {
+            int[] arr = [0, 9, 1, 8, 2, 7, 3, 6, 4, 5];
+            int[] sortedArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            Bubble.sort(arr, arr.Length);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(arr, Has.Length.EqualTo(10));
+                Assert.That(arr, Is.EqualTo(sortedArr));
+            }
+        }
+    }
+}
